5ee8004d6df01e53e5058a66f54da1d9edce05ad
Araceli <araceliteruel@gmail.com>
2015-05-06 21:39:55 +0200

    Creamos el test de la clase CadenaAlumno e implementamos el primer método

---
+++ unit9/test/unit9/CadenaAlumnoTest.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unit9;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Araceli Teruel
+ */
+public class CadenaAlumnoTest {
+    
+    public CadenaAlumnoTest() {
+    }
+
+    /**
+     * Test of sonIguales method, of class CadenaAlumno.
+     */
+    @Test
+    public void testSonIguales() {
+        boolean igualesDevuelta=CadenaAlumno.sonIguales("entornos", "ento");
+        boolean real="entornos".equals("ento");
+        assertEquals(real,igualesDevuelta);
+    }
+
+    /**
+     * Test of esMayor method, of class CadenaAlumno.
+     */
+    @Test
+    public void testEsMayor() {
+    }
+
+    /**
+     * Test of esMenor method, of class CadenaAlumno.
+     */
+    @Test
+    public void testEsMenor() {
+    }
+
+    /**
+     * Test of comparaIgnorandoMayusculas method, of class CadenaAlumno.
+     */
+    @Test
+    public void testComparaIgnorandoMayusculas() {
+    }
+
+    /**
+     * Test of reemplazaTodos method, of class CadenaAlumno.
+     */
+    @Test
+    public void testReemplazaTodos() {
+    }
+
+    /**
+     * Test of reemplazaPrimero method, of class CadenaAlumno.
+     */
+    @Test
+    public void testReemplazaPrimero() {
+    }
+
+    /**
+     * Test of muestraSplitLimite method, of class CadenaAlumno.
+     */
+    @Test
+    public void testMuestraSplitLimite() {
+    }
+
+    /**
+     * Test of muestraSplit method, of class CadenaAlumno.
+     */
+    @Test
+    public void testMuestraSplit() {
+    }
+
+    /**
+     * Test of quitaEspacios method, of class CadenaAlumno.
+     */
+    @Test
+    public void testQuitaEspacios() {
+    }
+
+    /**
+     * Test of convertirMayusculas method, of class CadenaAlumno.
+     */
+    @Test
+    public void testConvertirMayusculas() {
+    }
+
+    /**
+     * Test of convertirMinusculas method, of class CadenaAlumno.
+     */
+    @Test
+    public void testConvertirMinusculas() {
+    }
+
+    /**
+     * Test of longitudCadena method, of class CadenaAlumno.
+     */
+    @Test
+    public void testLongitudCadena() {
+    }
+
+    /**
+     * Test of empiezaCon method, of class CadenaAlumno.
+     */
+    @Test
+    public void testEmpiezaCon() {
+    }
+
+    /**
+     * Test of acabaEn method, of class CadenaAlumno.
+     */
+    @Test
+    public void testAcabaEn() {
+    }
+
+    /**
+     * Test of posicionPrimeraCadena method, of class CadenaAlumno.
+     */
+    @Test
+    public void testPosicionPrimeraCadena() {
+    }
+
+    /**
+     * Test of extraerSubstring method, of class CadenaAlumno.
+     */
+    @Test
+    public void testExtraerSubstring_String_int() {
+    }
+
+    /**
+     * Test of extraerSubstring method, of class CadenaAlumno.
+     */
+    @Test
+    public void testExtraerSubstring_3args() {
+    }
+
+    /**
+     * Test of concatenaCadenas method, of class CadenaAlumno.
+     */
+    @Test
+    public void testConcatenaCadenas() {
+    }
+    
+}
